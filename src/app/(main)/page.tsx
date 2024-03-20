dbee1bc1dec5d89af75b5a4a0a9c1aa9ffd32394
SatakeYusuke19920527 <satkyusk@icloud.com>
2024-03-20 13:01:31 +0900

    commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,23 +1,24 @@
-import Link from 'next/link';
-import { MdAddTask } from 'react-icons/md';
-import TaskCard from '../../components/TaskCard/TaskCard';
-import { Task } from '../../models/task';
-const getAllTasks = async (): Promise<Task[]> => {
-  const response = await fetch(`${process.env.LOCAL_URL}/api/tasks`, {
-    cache: 'no-store',
-  });
+// import Link from 'next/link';
+// import { MdAddTask } from 'react-icons/md';
+// import TaskCard from '../../components/TaskCard/TaskCard';
+// import { Task } from '../../models/task';
+// const getAllTasks = async (): Promise<Task[]> => {
+//   const response = await fetch(`${process.env.LOCAL_URL}/api/tasks`, {
+//     cache: 'no-store',
+//   });
 
-  if(response.status !== 200) throw new Error('Failed to fetch tasks');
+//   if(response.status !== 200) throw new Error('Failed to fetch tasks');
 
-  const data = await response.json();
-  return data.resources as Task[];
-}
+//   const data = await response.json();
+//   return data.resources as Task[];
+// }
 
 export default async function MainPage() {
-  const allTasks = await getAllTasks();
+  // const allTasks = await getAllTasks();
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
-      <header className="flex justify-between items-center">
+      <p>test</p>
+      {/* <header className="flex justify-between items-center">
         <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
         <Link
           href="/new"
@@ -31,7 +32,7 @@ export default async function MainPage() {
         {allTasks.map((task) => (
           <TaskCard key={task.id} {...task} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
