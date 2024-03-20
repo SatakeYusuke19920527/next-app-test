5399fe830e74a5b599806c1549f511c5bedcc09b
SatakeYusuke19920527 <satkyusk@icloud.com>
2024-03-20 13:43:53 +0900

    title変更

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import NavList from './NavList/NavList'
 
 const SideMenu = () => {
   return (
     <div className='w-56 pt-8 bg-gray-800 text-white'>
-      <h1 className='px-4 text-2xl font-bold'>Next Tasks</h1>
+      <h1 className='px-4 text-2xl font-bold'>AppRouter</h1>
       <NavList />
     </div>
   )
